534efd75e051564592237cebe5192239913878ed
Ananya Virwani <ananyavirwani255@gmail.com>
2026-09-01 12:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //your JS code here. If required.
 function daysOfYear(year){
 	if(year%4==0 && (year%400==0 || year%100!=0)){
-		return 365
+		return 366
 	}
 	else{
-		return 366
+		return 365
 	}
 }
 
 daysOfYear(2022)
+daysOfYear(2024)
+daysOfYear(1900)
+daysOfYear(2000)
